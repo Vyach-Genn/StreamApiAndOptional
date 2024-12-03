e843898a986a7432e5b36c2a410ed81e104ce512
vachg <vachgen@yandex.ru>
2024-12-03 22:29:52 +0300

    Stream_Api

---
+++ src/main/java/streamapiandoptional/StreamApiAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package streamapiandoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiAndOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiAndOptionalApplication.class, args);
+	}
+
+}
